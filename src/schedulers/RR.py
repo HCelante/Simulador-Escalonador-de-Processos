79fc618ae271675ff6d5daebc725afff1717948c
Hcelante <higor.celante@gmail.com>
2019-10-02 20:47:58 -0300

    RRupdate

--- src/schedulers/RR.py
+++ src/schedulers/RR.py
@@ -12,17 +12,20 @@ class RR:
     # o metodo invocador deve checar o procState a cada execucao                                              #
     # quando o status muda, o metodo invocador deve mudar o processo de fila ou posicao na fila               #
     def update_BCPQt(self, bcp): # em cada quantum atualiza as informações do bcp do processo em execucao     #
-        if ((bcp.procQT_act < self.Quantum) and (bcp.procCPUuse < bcp.proc_Coust )):
-            if (bcp.procQT_act == (self.Quantum -1)):
+        if ((bcp.procQT_act < self.Quantum) and (bcp.procCPUuse < bcp.proc_Coust )): # se tem quantum pra consumir 
+            if (bcp.procQT_act == (self.Quantum -1)):      # se esta para consumir seu ultimo quantum dessa execucao
+                bcp.procQT_act = bcp.procQT_act + 1        
+                bcp.procCPUuse = bcp.procCPUuse + 1
+                bcp.procState = 'pronto'                   # fica pronto
+            else:                                          # se nao
                 bcp.procQT_act = bcp.procQT_act + 1
                 bcp.procCPUuse = bcp.procCPUuse + 1
-                bcp.procState = 'pronto'
-            else:
-                bcp.procQT_act = bcp.procQT_act + 1
-                bcp.procCPUuse = bcp.procCPUuse + 1
-                bcp.procState = 'executando'
+                bcp.procState = 'executando'               # continua executando
         
-        elif(bcp.procCPUuse == bcp.proc_Coust ): # se ja usou tudo q precisava
-            bcp.procState = 'terminado'
+        elif(bcp.procCPUuse == bcp.proc_Coust ):           # se ja usou tudo q precisava
+            bcp.procState = 'terminado'                    # termina o processo
+        
+        else:
+            print("Nao consumido, conferir o codigo!")
 
              
