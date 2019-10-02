a5bdd14c8324feb823e3a5ffe5fac9ac8555057f
Hcelante <higor.celante@gmail.com>
2019-10-02 20:49:37 -0300

    confere

--- src/models/bcp.py
+++ src/models/bcp.py
@@ -8,18 +8,16 @@
 class BCP: # Bloco de Controle de processos
     def __init__(self, context):
 
-        # INFORMACOES DE PROGRAMA
-        self.progCounter = context[0]   # contador de programa recebe linhdas 0 das informações de contexto
-        self.progStatus = context[1]    # status do programa
-
         # INFORMACOES DE PROCESSO
+        self.proc_Coust = context[0]     # custo total do processo
+        self.procQt_act = context[1]     # quantum consumido parcialmente
         self.schedParam = context[2]    # parametros do schelude
         self.procParent = context[3]    # id do processo pai
         self.procCPUuse = context[4]    # tempo usado da cpu
         self.procGroup = context[5]     # grupo do processo
         self.procIniHr = context[6]     # momento que o processo foi iniciado
         self.procState = context[7]     # estado do processo
-        self.procPriority = context[8]      # prioridade do processo
+        self.procPriority = context[8]  # prioridade do processo
         self.procID = context[9]        # id do processo
  
     def set_procCPUuse(self, use):
