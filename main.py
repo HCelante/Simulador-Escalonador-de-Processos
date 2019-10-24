cf44ce904caadf8d19e46526cf6dd767a9d6394b
RoyaleCheese <royalebrendow@gmail.com>
2019-10-23 23:08:37 -0300

    fazendo a leitura do arquivo de processos

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # import src.Gerenciador
 import sys
 import array as arr
+from src.models.bcp import BCP
 
 def getConfigurations(filename): # pega o tempo de I/O de cada escalonador de um arquivo de configuração 
   scheduler = ""
@@ -41,10 +42,23 @@ def getConfigurations(filename): # pega o tempo de I/O de cada escalonador de um
           if(scheduler == 'SJF'):
             SJF.append(int(c))
 
-  return [[RR],[DNMC],[SJF]]
+  return [RR,DNMC,SJF]
 
+def getProcess(filename):   #Adiciona os processos a uma lista de processos
+  processList = []
+  singleProcess = []
+  
+  with open(filename) as f:
+    for val in f.read().splitlines():
+      for i in val.strip().split():
+        singleProcess.append(int(i))
+      processList.append(singleProcess)
+      singleProcess = []
+      
+  return processList
 
 def main():
     print(getConfigurations(sys.argv[1]))
+    print (getProcess(sys.argv[2]))
 
 main()
